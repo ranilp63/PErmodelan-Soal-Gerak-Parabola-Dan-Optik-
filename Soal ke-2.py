d5ba6754e7feb4d2014abf749a1fca972db297c2
ranilp63 <114718310+ranilp63@users.noreply.github.com>
2022-10-06 17:39:05 +0700

    Update Soal ke-2.py

--- Soal ke-2.py
+++ Soal ke-2.py
@@ -1,11 +1,28 @@
+#Soal 2
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Gunakan oerasi perhitungan fisika matematika untuk menghitung jarak fokus lensa (f)ndalam cm pada persamaan pembuat lensa 1/f = (n-1)[1/R1+1R2] dengan indek  n bias medium
-n = 1.50
-R1 = 20 
-R2 = 18 
-F = (n-1)*(1/R1+1/R2)
-F = 1/F
+alpha = np.radians(45)
+g = 9.8
+v0 = 10
 
-print(F)
+v0x = v0*np.cos(alpha)
+v0y = v0*np.sin(alpha)
+
+X = ((v0**2)*np.sin(2*alpha))/(2*g)
+print("Jarak Horizontal Maksimum = ",X," m")
+Y = ((v0**2)*(np.sin(alpha)**2))/(2*g)
+print("Jarak Vertikal Maksimum = ",Y," m")
+T = (2*v0*np.sin(alpha))/g
+print("Waktu Mencapai Jarak Horizontal Maksimum = ",T," s")
+print("\n")
+
+t = np.arange(0.0, T, 0.01)
+y = v0y*t - 0.5*g*t**2
+x = v0x*t
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set(xlabel='x (m)', ylabel= 'y (m)', title='Grafik Gerak Parabola')
+ax.grid()
+plt.show()
